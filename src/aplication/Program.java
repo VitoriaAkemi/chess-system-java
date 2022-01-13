a635142a5ab80fa2e24657f20fee3ef07f6c634e
Vitoria Akemi <rm88077@fiap.com.br>
2022-01-13 17:18:53 -0300

    deixando tabuleiro visivel

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -2,12 +2,16 @@ package aplication;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Board board = new Board(8, 8);
+		ChessMatch chessMath = new ChessMatch();
+		UI.printBoard(chessMath.getPieces());
+		
+		
 	}
 
 }
